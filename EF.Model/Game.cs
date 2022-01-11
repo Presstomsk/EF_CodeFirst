68a8c2740aa2720e0fb057f53f0d53e15778b245
Roman Permiakov <info@presstomsk.ru>
2022-01-12 00:01:52 +0700

    Создание модели данных

---
+++ EF.Model/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EF.Model
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Creator { get; set; }
+        public string Style { get; set; }
+        public string DateOfRelease { get; set; }
+    }
+}
