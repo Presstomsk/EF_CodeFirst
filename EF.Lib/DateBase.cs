9c3befe96d6a053caada2f0549f1593fc25d32b4
Roman Permiakov <info@presstomsk.ru>
2022-01-12 00:20:58 +0700

    Создание контекста данных

---
+++ EF.Lib/DateBase.cs
@@ -0,0 +1,31 @@
+﻿
+using EF.Model;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace EF.Lib
+{
+    public class DateBase : DbContext
+    {
+        public static string connectionStringFile = "/ConnectionString.txt";
+        public DbSet<Game> TabGames { get; set; }
+
+        protected DateBase() { }
+        public DateBase(DbContextOptions<DateBase> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public static DateBase Init()
+        {
+            var options = new DbContextOptionsBuilder<DateBase>().UseMySQL(unt).Options;
+            return new DateBase(options);
+        }
+
+        protected static string GetConnectionString()
+        {
+            var streamReader = new StreamReader(Directory.GetCurrentDirectory() + connectionStringFile);
+            return streamReader.ReadToEnd();
+        }
+
+    }
+}
--- EF.Model/Game.cs
+++ EF.Model/Game.cs
@@ -1,5 +1,4 @@
-﻿using System;
-
+﻿
 namespace EF.Model
 {
     public class Game
